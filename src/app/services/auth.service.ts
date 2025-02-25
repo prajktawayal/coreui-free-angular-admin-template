1d1e4b62bb75d193f7c499dbafee2bc5f0929f64
Prajakta Wayal <PrajaktaWayal@logicontech.com>
2025-02-25 15:35:34 +0530

    change auth.services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs'; // ✅ Import Observable
 
 @Injectable({
   providedIn: 'root'  // ✅ Ensure it's provided in root
@@ -11,6 +12,16 @@ export class AuthService {
 
   SavePlantMaster(master: { PlantId: string; plantName: string; FactoryId: string; Plantnumber: string }) {
     return this.http.post<{ message: string }>(`${this.baseserverurl}PlantMaster/PlantMaster`, master);
+  }
+
+  getPlantMaster(): Observable<any> {
+    return this.http.get<any>(`${this.baseserverurl}PlantMaster/AllPlantMaster`);
+  }
+  updatePlantMaster(payload: { PlantId: string; plantName: string; FactoryId: string; Plantnumber: string })  {
+    return this.http.post<{ message: string }>(`${this.baseserverurl}PlantMaster/UpdatePlantMaster`, payload);
+  }
+  deletePlantMaster(plantId:string) {
+    return this.http.delete<{ message: string }>(`${this.baseserverurl}PlantMaster/DeletePlantMaster/${plantId}`);
   }
    
 }
